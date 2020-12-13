683e10b2f04252fbd74b4ba45687b359c21903ff
athunkadi <caturadi.nu@gmail.com>
2020-12-13 22:52:28 +0700

    add errorHandler

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ if (process.env.NODE_ENV === "development") {
   require("dotenv").config();
 }
 
+const errorHandler = require('errorhandler')
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +10,7 @@ const router = require('./routes');
 const cors = require('cors');
 
 //body parser
+app.use(errorHandler());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
