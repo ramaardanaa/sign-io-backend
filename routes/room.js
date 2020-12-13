a0a2ae6027bc75d88669d361369a3f4cb1e72ab4
athunkadi <caturadi.nu@gmail.com>
2020-12-13 19:48:47 +0700

    config route

--- routes/room.js
+++ routes/room.js
@@ -1,7 +1,9 @@
+const RoomController = require('../controllers/roomContol');
+
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-  res.send("room")
-})
+router.get('/', RoomController.findAll);
+router.post('/', RoomController.addRoom);
+router.delete('/:id', RoomController.deleteRoom);
 
 module.exports = router;
