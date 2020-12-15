0c001ac795836353263aaf0ca9503933d2db4274
athunkadi <caturadi.nu@gmail.com>
2020-12-15 11:10:31 +0700

    repair errorHandlers

--- middlewares/errorHandlers.js
+++ middlewares/errorHandlers.js
@@ -1,17 +1,3 @@
-module.exports = function(err, req, res, next){
-  let status = 500;
-  let msg = err.name || 'Internal Server Error';
-
-  switch(msg){
-    case 'SequelizeValidationError':
-      status = 400;
-      msg = err.errors[0].message;
-      break;
-    case 'email/password not null':
-      status = 404;
-      msg = 'email/password not null';
-      break;
-
 module.exports = (err, req, res, next) => {
   let status = err.status || 500
   let msg = err.msg || 'internal server error'
