68fc0e6f3f5a72ffc0ed60e4c5f59c298fb66a8b
Nikolas Stefano <nz.stefano@gmail.com>
2020-12-17 13:52:35 +0700

    Add prediction filter

--- routes/sign.js
+++ routes/sign.js
@@ -7,17 +7,39 @@ const modeltest = "https://teachablemachine.withgoogle.com/models/r6BBk-hiN/";
 
 const model = new TeachableMachine({
   modelUrl: model1,
+  // modelUrl: modeltest,
 });
 
 router.post("/", async (req, res, next) => {
   console.log("masuk sign.js");
   const { url } = req.body;
+  console.log(url);
   try {
+    // const image = await load(url)
     const predictions = await model.classify({
+      // imageUrl:
+      //   "https://sign-io.s3.ap-southeast-1.amazonaws.com/1608128952552-1608128952685.jpeg",
+      // imageUrl:
+      //   "https://tensorflow-a-e.s3.jp-tok.cloud-object-storage.appdomain.cloud/DOG.png",
+      // imageUrl:
+      //   "https://tensorflow-a-e.s3.jp-tok.cloud-object-storage.appdomain.cloud/1608170751038-1608170750972.jpeg",
+      // ini file baru
+      // "https://sign-io-storage.s3-ap-southeast-1.amazonaws.com/DOG.png",
       imageUrl: url,
     });
     console.log(predictions);
-    res.status(200).json({ predictions });
+    let result;
+    let num = 0;
+    predictions?.forEach((value) => {
+      if (value.score >= 0 && value.score <= 1) {
+        if (value.score > num) {
+          num = value.score;
+          result = value.class;
+        }
+      }
+    });
+    console.log("result", result);
+    res.status(200).json({ predictions: result });
   } catch (err) {
     console.log(err);
     next(err);
